f9561c4de07a0ecaefb66c67533bf8a64a460601
JfHarris <3374@holbertonschool.com>
2021-11-02 13:19:05 -0700

    Edit #include

--- main.h
+++ main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(char c);
+int print_perc(void);
+
+#endif
