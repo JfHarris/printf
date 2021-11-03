6e57030f1d151cb0acff83cc4809a99c39ffe339
JfHarris <3374@holbertonschool.com>
2021-11-03 11:20:40 -0700

    Added struct and prototypes

--- main.h
+++ main.h
@@ -5,9 +5,24 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * struct frmt - checks formats
+ *@type: format specifier to print
+ *@f: print function
+ *
+ */
+typedef struct frmt
+{
+	char *type;
+	int(*f)();
+} frm_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(char c);
 int print_perc(void);
+int print_unint(va_list ap);
+int print_dint(va_list ap);
+int print_op(const char *format, frm_t *print_arr, va_list list);
 
 #endif
